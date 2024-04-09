5cab81570e05b29876fa318f491848861cb95a06
Kauê Schaefer de Andrade <128431286+kauesandrade@users.noreply.github.com>
2024-04-09 22:48:20 +0000

    create the component carousel

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CarouselComponent } from './carousel/carousel.component';
 
-
+const Components = [
+  CarouselComponent
+]
 
 @NgModule({
-  declarations: [],
+  exports: [
+    ...Components
+  ],
+  declarations: [
+    ...Components
+  ],
   imports: [
     CommonModule
   ]
