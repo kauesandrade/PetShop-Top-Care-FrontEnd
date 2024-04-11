96acdaf7e56ee5b501b4cd56edcdc30b097f7804
Kauê Schaefer de Andrade <128431286+kauesandrade@users.noreply.github.com>
2024-04-11 00:57:30 +0000

    doing the absolute button, prev and next

--- src/app/shared/carousel/carousel.component.ts
+++ src/app/shared/carousel/carousel.component.ts
@@ -1,6 +1,6 @@
 import { Component, ElementRef, AfterViewInit, OnDestroy } from '@angular/core';
-import EmblaCarousel from 'embla-carousel';
-
+import EmblaCarousel, { EmblaOptionsType } from 'embla-carousel';
+import { AxisOptionType } from 'embla-carousel/components/Axis';
 
 @Component({
   selector: 'app-carousel',
@@ -9,54 +9,23 @@ import EmblaCarousel from 'embla-carousel';
 })
 export class CarouselComponent implements AfterViewInit, OnDestroy {
 
-  private embla: any = null;
-
   constructor(private elementRef: ElementRef) { }
-
+  
   ngAfterViewInit(): void {
-    const emblaNode = this.elementRef.nativeElement.querySelector('.embla');
     const viewportNode = this.elementRef.nativeElement.querySelector('.embla__viewport');
-    const prevBtnNode = this.elementRef.nativeElement.querySelector('.embla__button--prev');
-    const nextBtnNode = this.elementRef.nativeElement.querySelector('.embla__button--next');
-    const dotsNode = this.elementRef.nativeElement.querySelector('.embla__dots');
-
-    const OPTIONS = { loop: true };
-
+    const OPTIONS: EmblaOptionsType = {axis: 'x', loop: true }
+    
     this.embla = EmblaCarousel(viewportNode, OPTIONS);
-
-    if (this.embla) {
-      const removePrevNextBtnsClickHandlers = this.addPrevNextBtnsClickHandlers(prevBtnNode, nextBtnNode);
-
-      this.embla.on('destroy', removePrevNextBtnsClickHandlers);
-    }
   }
+  
+  embla: any = null;
+
+  itens: Array<string> = ['1','2','3']
 
   ngOnDestroy(): void {
     if (this.embla) {
       this.embla.destroy();
     }
-  }
-
-  private addPrevNextBtnsClickHandlers(prevBtnNode: HTMLElement, nextBtnNode: HTMLElement): () => void {
-    const prevBtnClickHandler = () => {
-      if (this.embla) {
-        this.embla.scrollPrev();
-      }
-    };
-
-    const nextBtnClickHandler = () => {
-      if (this.embla) {
-        this.embla.scrollNext();
-      }
-    };
-
-    prevBtnNode.addEventListener('click', prevBtnClickHandler);
-    nextBtnNode.addEventListener('click', nextBtnClickHandler);
-
-    return () => {
-      prevBtnNode.removeEventListener('click', prevBtnClickHandler);
-      nextBtnNode.removeEventListener('click', nextBtnClickHandler);
-    };
   }
 
   prevSlide(): void {
@@ -72,3 +41,4 @@ export class CarouselComponent implements AfterViewInit, OnDestroy {
   }
 
 }
+
