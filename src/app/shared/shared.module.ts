22e2072dbd5fb034271dd973e485580d5cb2068c
Gabriel-Baseggio <baseggiogabriel6@gmail.com>
2024-04-11 20:35:58 -0300

    Created ProductSectionComponent and styling adjustments to ProductCardComponent

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,8 +3,9 @@ import { CommonModule } from '@angular/common';
 import { ProductCardComponent } from './product-card/product-card.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { CoreModule } from '../core/core.module';
+import { ProductSectionComponent } from './product-section/product-section.component';
 
-const Components: any[] = [ProductCardComponent];
+const Components: any[] = [ProductCardComponent, ProductSectionComponent];
 
 @NgModule({
   exports: [...Components],
