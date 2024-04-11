7386b948bb7a978e6495eab5c42066163de4f7de
bino1432 <bernardozplz@gmail.com>
2024-04-11 20:57:19 -0300

    Select Input Finished

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './button/button.component';
+import { SelectInputComponent } from './select-input/select-input.component';
+import { TextAreaComponent } from './text-area/text-area.component';
 
-const Components = [ButtonComponent];
+const Components = [ButtonComponent, SelectInputComponent, TextAreaComponent];
 
 @NgModule({
   exports: [...Components],
---
+++ src/app/core/select-input/select-input.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-select-input',
+  templateUrl: './select-input.component.html',
+  styleUrls: ['./select-input.component.scss']
+})
+
+export class SelectInputComponent {
+
+  @Input() opcoes: string[] = ['Homem', 'Mulher', 'Prefiro não dizer'];
+
+  constructor() { }
+
+}
