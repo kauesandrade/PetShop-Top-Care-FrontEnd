6e5bd037a2aeaf430d312b005a5b80be23eba5ca
Gabriel-Baseggio <baseggiogabriel6@gmail.com>
2024-04-11 16:41:55 -0300

    Changing base scss structure and creating ProductCardComponent

--- src/app/shared/product-card/product-card.component.ts
+++ src/app/shared/product-card/product-card.component.ts
@@ -16,22 +16,11 @@ export class ProductCardComponent {
   faStar = faStar;
   faShoppingCart = faShoppingCart;
 
-  @Input() product?: Product = {
-    imgSrc:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQKb_VrQ8qleqISIXNNjCwAQtly_KKByH02OIGqzz6Qg&s',
-    favorite: true,
-    title: 'Ração úmida kelcat sachê carne',
-    brand: 'Kelcat',
-    rating: 4.99,
-    price: 15.54,
-    discountPrice: 5.54,
-    maxInterestFreeParcels: 5,
-    subscribersPrice: 3.54,
-  };
+  @Input() product!: Product;
 
   constructor() {}
 
-  test() {
-    console.log('XD');
+  onClick() {
+    console.log('clicked');
   }
 }
