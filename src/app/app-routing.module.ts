0c8a0f64234a329fbe6b107031ee00cf8d32769c
Kauê Schaefer de Andrade <128431286+kauesandrade@users.noreply.github.com>
2024-04-12 20:49:28 -0300

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'contatos', component:  },
+  { path: 'nossas-lojas', component:  },
+  { path: 'sobre-nos', component:  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
