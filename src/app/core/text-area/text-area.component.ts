33e3589c41fc70bb4c9ba07f2e2cc2ff15af1f4e
Bernardo P Zapelini <bernardozplz@gmail.com>
2024-04-12 20:46:38 -0300

    Finished Text Area

--- src/app/core/text-area/text-area.component.ts
+++ src/app/core/text-area/text-area.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-text-area',
   templateUrl: './text-area.component.html',
   styleUrls: ['./text-area.component.scss']
 })
-export class TextAreaComponent implements OnInit {
+export class TextAreaComponent {
+
+  @Input() width: string = "0"
+
+  @Input() placeholder: string = ""
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
