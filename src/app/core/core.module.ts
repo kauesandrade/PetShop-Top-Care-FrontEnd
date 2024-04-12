42cb95d19718bfe49192a958c84f0df662a8add8
Gabriel Baseggio <gabriel_baseggio@estudante.sc.senai.br>
2024-04-12 20:42:02 -0300

    Working on the FooterComponent

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,12 +4,18 @@ import { ButtonComponent } from './button/button.component';
 import { FooterComponent } from './footer/footer.component';
 import { FooterMainComponent } from './footer/footer-main/footer-main.component';
 import { FooterCopyrightComponent } from './footer/footer-copyright/footer-copyright.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
-const Components = [ButtonComponent, FooterComponent];
+const Components = [
+  ButtonComponent,
+  FooterComponent,
+  FooterMainComponent,
+  FooterCopyrightComponent,
+];
 
 @NgModule({
   exports: [...Components],
-  declarations: [...Components, FooterMainComponent, FooterCopyrightComponent],
-  imports: [CommonModule],
+  declarations: [...Components],
+  imports: [CommonModule, FontAwesomeModule],
 })
 export class CoreModule {}
--- src/app/core/footer/footer-main/footer-main.component.ts
+++ src/app/core/footer/footer-main/footer-main.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {
+  faFacebook,
+  faFacebookSquare,
+  faInstagram,
+  faLinkedin,
+  faPinterest,
+  faTiktok,
+  faTwitter,
+  faWhatsapp,
+  faYoutube,
+} from '@fortawesome/free-brands-svg-icons';
+import { faCommentDots, faEnvelope } from '@fortawesome/free-regular-svg-icons';
+import { faPhone } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-footer-main',
   templateUrl: './footer-main.component.html',
-  styleUrls: ['./footer-main.component.scss']
+  styleUrls: ['./footer-main.component.scss'],
 })
-export class FooterMainComponent implements OnInit {
+export class FooterMainComponent {
+  faWhatsapp = faWhatsapp;
+  faEnvelope = faEnvelope;
+  faPhone = faPhone;
+  faCommentDots = faCommentDots;
 
-  constructor() { }
+  faFacebook = faFacebookSquare;
+  faInstagram = faInstagram;
+  faLinkedin = faLinkedin;
+  faYoutube = faYoutube;
+  faTiktok = faTiktok;
+  faPinterest = faPinterest;
+  faTwitter = faTwitter;
 
-  ngOnInit(): void {
-  }
-
+  constructor() {}
 }
