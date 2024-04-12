c534e77335adb30a4bb5665a02f2b6798c16b9cc
Gabriel-Baseggio <baseggiogabriel6@gmail.com>
2024-04-11 21:56:00 -0300

    Created FooterComponent

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './button/button.component';
+import { FooterComponent } from './footer/footer.component';
+import { FooterMainComponent } from './footer/footer-main/footer-main.component';
+import { FooterCopyrightComponent } from './footer/footer-copyright/footer-copyright.component';
 
-const Components = [ButtonComponent];
+const Components = [ButtonComponent, FooterComponent];
 
 @NgModule({
   exports: [...Components],
-  declarations: [...Components],
+  declarations: [...Components, FooterMainComponent, FooterCopyrightComponent],
   imports: [CommonModule],
 })
 export class CoreModule {}
