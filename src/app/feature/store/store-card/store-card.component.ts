7b5428fdd838799ef2def1a7b042fb18e445b424
Kauê Schaefer de Andrade <128431286+kauesandrade@users.noreply.github.com>
2024-04-15 20:51:11 -0300

    reviewed

--- src/app/feature/store/store-card/store-card.component.ts
+++ src/app/feature/store/store-card/store-card.component.ts
@@ -13,19 +13,6 @@ export class StoreCardComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  @Input() store: StoreCard = {
-    imgSrc: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-D_dE33FFe8BA-ejROqrhBClDmglc3oZkTOZR6fqv_Q&s",
-    link: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-D_dE33FFe8BA-ejROqrhBClDmglc3oZkTOZR6fqv_Q&s",
-    name: "TopCare Jaraguá do Sul",
-    location: "Av. Barão do Rio Branco, Nº 5102",
-    number: "(99) 9 9999-9999",
-    opening_hours: "7:00 ás 22:00",
-    services: [
-      "Banho e Tosa",
-      "Consulta Veterinária",
-      "Adoção"
-    ],
-    products_offered: "Animais Domésticos",
-  };
+  @Input() store!: StoreCard
 
 }
