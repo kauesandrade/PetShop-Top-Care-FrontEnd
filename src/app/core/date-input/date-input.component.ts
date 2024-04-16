15ff85e2ee7b652153b8af177fb8d46480c4b7ff
Bernardo P Zapelini <bernardozplz@gmail.com>
2024-04-15 21:51:40 -0300

    Update DateInputComponent

--- src/app/core/date-input/date-input.component.ts
+++ src/app/core/date-input/date-input.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-date-input',
   templateUrl: './date-input.component.html',
   styleUrls: ['./date-input.component.scss']
 })
-export class DateInputComponent implements OnInit {
+export class DateInputComponent {
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
