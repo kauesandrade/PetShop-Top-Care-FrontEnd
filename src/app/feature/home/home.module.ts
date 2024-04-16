57b4f01d9116df28f230633b5ef464c2ab46aaf9
Gabriel Baseggio <gabriel_baseggio@estudante.sc.senai.br>
2024-04-16 17:00:26 -0300

    Making HomePage responsive

--- src/app/feature/home/home.module.ts
+++ src/app/feature/home/home.module.ts
@@ -4,10 +4,17 @@ import { HomeComponent } from './home.component';
 import { RoundCardComponent } from './components/round-card/round-card.component';
 import { ChatButtonComponent } from './components/chat-button/chat-button.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HomeBannerComponent } from './components/home-banner/home-banner.component';
+import { CoreModule } from 'src/app/core/core.module';
 
 @NgModule({
   exports: [HomeComponent],
-  declarations: [HomeComponent, RoundCardComponent, ChatButtonComponent],
-  imports: [CommonModule, FontAwesomeModule],
+  declarations: [
+    HomeComponent,
+    RoundCardComponent,
+    ChatButtonComponent,
+    HomeBannerComponent,
+  ],
+  imports: [CommonModule, FontAwesomeModule, CoreModule],
 })
 export class HomeModule {}
