7a9ad9851a9d1b5cd11e20559a8281d84f03fb7e
Gabriel Baseggio <gabriel_baseggio@estudante.sc.senai.br>
2024-04-24 18:35:16 -0300

    Creating breakpoints variables for responsiveness

--- src/app/shared/product-carousel/product-carousel.component.ts
+++ src/app/shared/product-carousel/product-carousel.component.ts
@@ -1,4 +1,10 @@
-import { Component, ElementRef, AfterViewInit, OnDestroy, Input } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  AfterViewInit,
+  OnDestroy,
+  Input,
+} from '@angular/core';
 import EmblaCarousel, { EmblaOptionsType } from 'embla-carousel';
 import { Product } from '../interfaces/product';
 import { faAngleLeft } from '@fortawesome/free-solid-svg-icons';
@@ -7,22 +13,22 @@ import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-product-carousel',
   templateUrl: './product-carousel.component.html',
-  styleUrls: ['./product-carousel.component.scss']
+  styleUrls: ['./product-carousel.component.scss'],
 })
 export class ProductCarouselComponent implements AfterViewInit, OnDestroy {
-
   constructor(private elementRef: ElementRef) {}
-  
+
   ngAfterViewInit(): void {
-    const viewportNode = this.elementRef.nativeElement.querySelector('.embla__viewport');
-    const OPTIONS: EmblaOptionsType = {align: 'start', loop: true};
-    
+    const viewportNode =
+      this.elementRef.nativeElement.querySelector('.embla__viewport');
+    const OPTIONS: EmblaOptionsType = { align: 'start', loop: true };
+
     this.embla = EmblaCarousel(viewportNode, OPTIONS);
   }
-  
+
   embla: any = null;
-  faAngleLeft = faAngleLeft
-  faAngleRight = faAngleRight
+  faAngleLeft = faAngleLeft;
+  faAngleRight = faAngleRight;
 
   @Input() itensProduct?: Array<Product>;
 
@@ -43,5 +49,4 @@ export class ProductCarouselComponent implements AfterViewInit, OnDestroy {
       this.embla.scrollNext();
     }
   }
-
 }
