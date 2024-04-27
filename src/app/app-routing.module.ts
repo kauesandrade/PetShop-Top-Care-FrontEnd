487b8d748de481ee0cefb5df96241b075273fdb5
Kauê Schaefer de Andrade <128431286+kauesandrade@users.noreply.github.com>
2024-04-27 16:13:53 -0300

    add title in pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,10 +6,10 @@ import { ContactComponent } from './feature/contact/contact.component';
 import { StoreComponent } from './feature/store/store.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'sobre-nos', component: AboutUsComponent },
-  { path: 'contate-nos', component: ContactComponent },
-  { path: 'nossas-lojas', component: StoreComponent },
+  { path: '', title: 'Home | Top Care', component: HomeComponent },
+  { path: 'sobre-nos', title: 'Sobre a Top Care', component: AboutUsComponent },
+  { path: 'contate-nos', title: 'Contate-nos | Top Care', component: ContactComponent },
+  { path: 'nossas-lojas', title: 'Lojas | Top Care', component: StoreComponent },
 ];
 
 @NgModule({
