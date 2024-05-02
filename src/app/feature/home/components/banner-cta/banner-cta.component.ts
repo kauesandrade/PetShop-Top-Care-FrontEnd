acfe834af48f961665dd67eedf9b1957d028c29a
Kauê Schaefer de Andrade <128431286+kauesandrade@users.noreply.github.com>
2024-05-02 15:11:15 -0300

    need review banner-cta

--- src/app/feature/home/components/banner-cta/banner-cta.component.ts
+++ src/app/feature/home/components/banner-cta/banner-cta.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-banner-cta',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./banner-cta.component.scss']
 })
 export class BannerCtaComponent implements OnInit {
-
-  constructor() { }
+  
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
+  }
+  
+  openPage(page: string) {
+    this.router.navigate([page]);
   }
 
 }
