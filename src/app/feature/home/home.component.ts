c925c087aa64dbe4b81d23ed901d7aa3c88a5dc6
Gabriel Baseggio <gabriel_baseggio@estudante.sc.senai.br>
2024-05-06 16:22:01 -0300

    Adjusting ProductCardSection

--- src/app/feature/home/home.component.ts
+++ src/app/feature/home/home.component.ts
@@ -15,38 +15,37 @@ export class HomeComponent implements OnInit {
 
   product: any = productData;
   products: any = this.product.product;
-  
-  itens: Array<MainCarousel> = 
-  [
-  {
-    imgSrc: "../assets/1.png",
-    link: "",
-    alt: "card 1"
-  },
-  {
-    imgSrc: "../assets/2.png",
-    link: "",
-    alt: "card 2"
-  },
-  {
-    imgSrc: "../assets/3.png",
-    link: "",
-    alt: "card 3"
-  },
-  {
-    imgSrc: "../assets/4.png",
-    link: "/sobre-nos",
-    alt: "card 4"
-  },
-  {
-    imgSrc: "../assets/5.png",
-    link: "/nossas-lojas",
-    alt: "card 5"
-  },
-  {
-    imgSrc: "../assets/6.png",
-    link: "contate-nos",
-    alt: "card 6"
-  },
-];
+
+  itens: Array<MainCarousel> = [
+    {
+      imgSrc: '../assets/1.png',
+      link: '',
+      alt: 'card 1',
+    },
+    {
+      imgSrc: '../assets/2.png',
+      link: '',
+      alt: 'card 2',
+    },
+    {
+      imgSrc: '../assets/3.png',
+      link: '',
+      alt: 'card 3',
+    },
+    {
+      imgSrc: '../assets/4.png',
+      link: '/sobre-nos',
+      alt: 'card 4',
+    },
+    {
+      imgSrc: '../assets/5.png',
+      link: '/nossas-lojas',
+      alt: 'card 5',
+    },
+    {
+      imgSrc: '../assets/6.png',
+      link: 'contate-nos',
+      alt: 'card 6',
+    },
+  ];
 }
