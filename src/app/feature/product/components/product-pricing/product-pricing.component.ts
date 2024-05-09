ed8db4ca784285bb4490aceaa8c344af82283418
Bernardo P Zapelini <bernardozplz@gmail.com>
2024-05-08 21:58:31 -0300

    init scss in productPricing

--- src/app/feature/product/components/product-pricing/product-pricing.component.ts
+++ src/app/feature/product/components/product-pricing/product-pricing.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Product } from 'src/app/shared/interfaces/product';
+import { faCheck, faPercent, faPlus, faMinus } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-product-pricing',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-pricing.component.scss']
 })
 export class ProductPricingComponent implements OnInit {
+  
+  @Input() product?: Product
+
+  faCheck = faCheck;
+  faPercent = faPercent;
+  faPlus = faPlus;
+  faMinus = faMinus;
 
   constructor() { }
 
   ngOnInit(): void {
   }
+  
+
 
 }
