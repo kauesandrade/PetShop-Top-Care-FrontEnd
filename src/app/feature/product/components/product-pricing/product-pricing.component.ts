6eec82dfc682e9f66dc5c4850c451bb8603be351
Kauê Schaefer de Andrade <128431286+kauesandrade@users.noreply.github.com>
2024-05-09 16:27:57 -0300

    finish style productPricing

--- src/app/feature/product/components/product-pricing/product-pricing.component.ts
+++ src/app/feature/product/components/product-pricing/product-pricing.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit } from '@angular/core';
 import { Product } from 'src/app/shared/interfaces/product';
 import { faCheck, faPercent, faPlus, faMinus } from '@fortawesome/free-solid-svg-icons';
+// import * as Inputmask from 'inputmask';
 
 @Component({
   selector: 'app-product-pricing',
@@ -11,16 +12,27 @@ export class ProductPricingComponent implements OnInit {
   
   @Input() product?: Product
 
+  // private regexMap = {integer: '^[0-9]*$',};
+
   faCheck = faCheck;
   faPercent = faPercent;
   faPlus = faPlus;
   faMinus = faMinus;
 
+  value: number = 1;
+
   constructor() { }
 
   ngOnInit(): void {
   }
-  
 
+  plusValue(){
+    this.value++
+  }
 
+  minusValue(){
+    if(this.value > 1){
+      this.value--
+    }
+  }
 }
