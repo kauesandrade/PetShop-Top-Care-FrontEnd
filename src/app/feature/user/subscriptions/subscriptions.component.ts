4266ed808175732d5c30cad4aab5917b4cc4dd22
Gabriel Baseggio <gabriel_baseggio@estudante.sc.senai.br>
2024-05-10 16:58:13 -0300

    Adjusting layout and hiding cards to be done later

--- src/app/feature/user/subscriptions/subscriptions.component.ts
+++ src/app/feature/user/subscriptions/subscriptions.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-subscriptions',
   templateUrl: './subscriptions.component.html',
-  styleUrls: ['./subscriptions.component.scss']
+  styleUrls: ['./subscriptions.component.scss'],
 })
 export class SubscriptionsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
