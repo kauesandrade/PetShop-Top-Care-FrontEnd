e7360f281575c9d48bbb15e2aa163826bc05ad0f
Gabriel-Baseggio <gabriel_baseggio@estudante.sc.senai.br>
2024-05-18 10:52:13 -0300

    Adjusting forgot password position

--- src/app/feature/login/login.component.ts
+++ src/app/feature/login/login.component.ts
@@ -12,6 +12,6 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {}
 
   openRegister() {
-    this.router.navigate(['/register']);
+    this.router.navigate(['/registro']);
   }
 }
