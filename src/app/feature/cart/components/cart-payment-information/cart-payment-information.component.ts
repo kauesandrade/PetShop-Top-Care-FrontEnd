05238ca3bb1c79497728d849bb0f6ffee539be2a
Kauê Schaefer de Andrade <128431286+kauesandrade@users.noreply.github.com>
2024-05-17 21:56:10 -0300

    init cartInformations

--- src/app/feature/cart/components/cart-payment-information/cart-payment-information.component.ts
+++ src/app/feature/cart/components/cart-payment-information/cart-payment-information.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subject, takeUntil } from 'rxjs';
+import { Item } from 'src/app/shared/interfaces/order/item';
+import { CartService } from 'src/app/shared/services/cart/cart.service';
 
 @Component({
   selector: 'app-cart-payment-information',
   templateUrl: './cart-payment-information.component.html',
   styleUrls: ['./cart-payment-information.component.scss']
 })
-export class CartPaymentInformationComponent implements OnInit {
+export class CartPaymentInformationComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  itens: Array<Item> = []
+
+  total: number = 0;
+  order: number = 0;
+
+  private unsubscribe = new Subject<void>();
+
+  constructor(private cartService: CartService, private route: ActivatedRoute, private router: Router) { }
+
 
   ngOnInit(): void {
+    this.itens = this.cartService.itensCart;
+    this.order = this.cartService.sumProductPrices(this.itens);
+    this.cartService.getItens().pipe(takeUntil(this.unsubscribe)).subscribe(data => {
+      this.itens = data;
+      this.order = this.cartService.sumProductPrices(this.itens);
+    });
   }
+
+  handleClickBuy() {
+
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe.next();
+    this.unsubscribe.complete();
+  }
+
+  handleClickGoback() {
+    this.router.navigate(['..'], { relativeTo: this.route });
+  }
+
+
 
 }
