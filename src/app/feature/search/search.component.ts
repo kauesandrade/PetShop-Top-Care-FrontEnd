6899cc61d257e242ef7a250eee8cd7f7a3dbc6b7
Gabriel Baseggio <gabriel_baseggio@estudante.sc.senai.br>
2024-05-27 17:50:52 -0300

    Header categories going to search page

--- src/app/feature/search/search.component.ts
+++ src/app/feature/search/search.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { filter } from 'rxjs';
 import { ProductVariant } from 'src/app/shared/interfaces/product/product-variant';
 import { Filter } from 'src/app/shared/interfaces/search/filter';
 import { FilterService } from 'src/app/shared/services/filter/filter.service';
@@ -15,6 +16,7 @@ export class SearchComponent implements OnInit {
   productsList: Array<ProductVariant> = [];
   productFilters: Array<Filter> = [];
   searchBy!: string;
+  filterBy?: string;
 
   constructor(
     private route: ActivatedRoute,
@@ -25,9 +27,23 @@ export class SearchComponent implements OnInit {
 
   ngOnInit(): void {
     this.route.queryParams.subscribe((params) => {
-      this.searchBy = params['q'].replace('%20', ' ');
-      this.productsList = this.searchService.searchProducts(this.searchBy);
-      this.productFilters = this.filterService.getListFilterWithChecked(this.productsList);
+      this.filterBy = params['categoria'];
+
+      if (this.filterBy) {
+        this.filterBy =
+          this.filterBy.charAt(0).toUpperCase() + this.filterBy.slice(1);
+        this.searchBy = this.filterBy;
+
+        this.productsList = this.filterService.getAllProductsOfCategory([
+          this.filterBy,
+        ]);
+      } else {
+        this.searchBy = params['q'].replace('%20', ' ');
+        this.productsList = this.searchService.searchProducts(this.searchBy);
+      }
+      this.productFilters = this.filterService.getListFilterWithChecked(
+        this.productsList
+      );
     });
   }
 
@@ -36,10 +52,17 @@ export class SearchComponent implements OnInit {
   }
 
   getFilters(evt: Array<string>) {
-    this.productsList = this.filterService.filterProducts(
-      evt,
-      this.searchService.getProductList()
-    );
+    if (this.filterBy) {
+      this.productsList = this.filterService.filterProducts(
+        evt,
+        this.filterService.getAllProductsOfCategory([this.filterBy])
+      );
+    } else {
+      this.productsList = this.filterService.filterProducts(
+        evt,
+        this.searchService.getProductList()
+      );
+    }
   }
 
   getFiltersProducts() {
