8a66b5a52bbc2ed9ade2b8b23fdc9e95ea737aab
Gabriel Baseggio <gabriel_baseggio@estudante.sc.senai.br>
2024-06-14 14:36:30 -0300

    Installing toastr library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 
 import { registerLocaleData } from '@angular/common';
 import ptBr from '@angular/common/locales/pt';
@@ -22,6 +24,23 @@ registerLocaleData(ptBr);
     SharedModule,
     FeatureModule,
     FontAwesomeModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      closeButton: true,
+      preventDuplicates: true,
+      newestOnTop: true,
+      progressBar: true,
+      progressAnimation: 'decreasing',
+      timeOut: 3000,
+      disableTimeOut: 'extendedTimeOut',
+      maxOpened: 2,
+      easing: 'ease-in',
+      easeTime: 300,
+      positionClass: 'toast-top-right',
+      toastClass: 'ngx-toastr toastr',
+      titleClass: 'toast-title toastr-title',
+      messageClass: 'toast-message toastr-message',
+    }),
   ],
   providers: [{ provide: LOCALE_ID, useValue: 'pt-BR' }],
   bootstrap: [AppComponent],
