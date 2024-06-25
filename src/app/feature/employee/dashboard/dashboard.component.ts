a71abb633dee64b14d373401794eee7f2bead605
kauesandrade <128431286+kauesandrade@users.noreply.github.com>
2024-06-24 21:56:26 -0300

    finish dashboardPage , need to do a resize

--- src/app/feature/employee/dashboard/dashboard.component.ts
+++ src/app/feature/employee/dashboard/dashboard.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import Chart from 'chart.js/auto';
 import { ProductVariant } from 'src/app/shared/interfaces/product/product-variant';
+import { ServiceVariant } from 'src/app/shared/interfaces/services/service-variant';
 import { SearchService } from 'src/app/shared/services/search/search.service';
 
 @Component({
@@ -8,24 +10,59 @@ import { SearchService } from 'src/app/shared/services/search/search.service';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit{
+
+  chart: any = [];
+  productList: Array<ProductVariant> = []
+  service!: ServiceVariant
+
+  faSearch = faSearch;
+
+  isOpen: boolean = false;
+
+  labels: any = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez']
+  data: any = {
+    labels: this.labels,
+    datasets: [{
+      label: 'Produtos vendidos',
+      data: [65, 59, 80, 81, 56, 55, 40, 1000],
+      fill: false,
+      borderColor: 'rgb(255, 176, 31)',
+      tension: 0.1
+    }]
+  };
+
 
   constructor(private searchService: SearchService) { }
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.chart.update();
+  }
+
 
   ngOnInit(): void {
     this.searchService.searchProducts("");
     this.productList = this.searchService.getProductList();
+
+    this.chart = new Chart('canvas', {
+      type: 'line',
+      data: this.data,
+      options: {
+        responsive: true,
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+
+      },
+    });
+
   }
 
-  productList: Array<ProductVariant> = []
-
-  faSearch = faSearch;
-  
-  isOpen: boolean = false;
-
-  sideBarOpen(evt: any){
+  sideBarOpen(evt: any) {
     this.isOpen = evt;
   }
-
-
 }
+
