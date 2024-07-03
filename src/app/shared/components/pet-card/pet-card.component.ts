a2500999e710dcd470b2c571e50d5cee661a2831
Bernardo P Zapelini <bernardozplz@gmail.com>
2024-07-03 20:01:23 -0300

    PetProfile Finished

--- src/app/shared/components/pet-card/pet-card.component.ts
+++ src/app/shared/components/pet-card/pet-card.component.ts
@@ -9,7 +9,7 @@ import { Pet } from '../../interfaces/pet/pet';
   styleUrls: ['./pet-card.component.scss'],
 })
 export class PetCardComponent implements OnInit {
-  @Input() type: string = 'link';
+  @Input() type: 'add' | 'link' | 'scheduling' = 'link';
   @Input() pet!: Pet;
 
   @Input() scheduleSelected?: Pet;
