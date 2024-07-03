73209559a10dbbea11e5d161155e368cddb72c98
kauesandrade <128431286+kauesandrade@users.noreply.github.com>
2024-07-02 21:57:59 -0300

    apresentation variantForms

--- src/app/feature/employee/dashboard/dashboard-page-product/components/variant-forms/variant-forms.component.ts
+++ src/app/feature/employee/dashboard/dashboard-page-product/components/variant-forms/variant-forms.component.ts
@@ -19,17 +19,19 @@ export class VariantFormsComponent implements OnInit {
 
   variationsOpen = false;
 
+  variantUpdate = "";
+
   variantForm = this.formBuilder.group({
     title: [''],
-    code: [''],
+    code: [0],
     stock: [''],
-    price: [''],
-    images: [''],
+    price: [0],
+    images: ['']
   })
 
   // variantFormArray = new FormArray([this.variantForm]);
 
-  
+
   files: Array<File> = []
 
   constructor(private formBuilder: FormBuilder) { }
@@ -37,14 +39,10 @@ export class VariantFormsComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  addVariant() {
-    this.variationsOpen = !this.variationsOpen;
-  }
-
   addImagesVariant(evt: any) {
     const fis: Array<File> = evt.target.files
 
-    for(let i = 0; i < fis.length; i++){
+    for (let i = 0; i < fis.length; i++) {
       this.files.push(fis[i]);
     }
 
@@ -53,32 +51,84 @@ export class VariantFormsComponent implements OnInit {
     // reader.onload = (_event) => {
     //   console.log(reader.result!);
     // };
-    
-    
+
+
     // this.variantForm.controls.images.setValue(files);
-    
+
   }
-  
-  getImage(img: File){
-    
+
+  getImage(img: File) {
+
     const reader = new FileReader();
     reader.readAsDataURL(img);
     reader.onloadend = (_event) => {
-        // console.log(reader.result!);
-        // return reader.result!;
-      };
-    
+      // console.log(reader.result!);
+      // return reader.result!;
+    };
+
   }
 
 
-  deleteVariant(variant: ProductVariant){
-    this.productVariantsList?.forEach(productVariant =>{
-      if(productVariant.variantCode == variant.variantCode){
-        this.productVariantsList?.splice(this.productVariantsList?.indexOf(productVariant), 1);
+  addSpecifications() {
+    this.variationsOpen = true;
+    this.variantUpdate = "";
+    this.variantForm = this.formBuilder.group({
+      title: [''],
+      code: [0],
+      stock: [''],
+      price: [0],
+      images: ['']
+    })
+  }
+
+  deleteVariant(productVariant: ProductVariant) {
+    this.productVariantsList.forEach(variant => {
+      if (variant.variantCode == productVariant.variantCode) {
+       this.productVariantsList.splice(this.productVariantsList.indexOf(variant), 1);
+        if (this.variantForm.value.code == productVariant.variantCode) {
+          this.variationsOpen = false;
+        }
       }
     })
-
     //ADD NO product update
+  }
+
+  editSpecification(productVariant: ProductVariant) {
+    this.variantUpdate = productVariant.variant!
+    this.variationsOpen = true;
+    this.variantForm = this.formBuilder.group({
+      title: [productVariant.variant],
+      code: [productVariant.variantCode],
+      stock: [''],
+      price: [productVariant.price],
+      images: ['']
+    })
+  }
+
+  updateVariant() {
+    this.productVariantsList.forEach(productVariant => {
+      if (productVariant.variant == this.variantUpdate) {
+        this.productVariantsList[this.productVariantsList.indexOf(productVariant)].variant = this.variantForm.value.title!
+        this.productVariantsList[this.productVariantsList.indexOf(productVariant)].variantCode = this.variantForm.value.code!
+        this.productVariantsList[this.productVariantsList.indexOf(productVariant)].price = this.variantForm.value.price!
+        // this.productVariantsList[this.productVariantsList.indexOf(productVariant)].stock = this.variantForm.value.stock!
+        // this.productVariantsList[this.productVariantsList.indexOf(productVariant)].images = this.variantForm.value.images!
+        this.variantUpdate = this.variantForm.value.title!
+      }
+    })
+  }
+
+  addVariant() {
+    // let productVariant: Pro = {
+    //   title: this.variantForm.value.title!,
+    //   code: this.variantForm.value.code!,
+    //   stock: [''],
+    //   price: this.variantForm.value.price!,
+    //   images: ['']
+    // }
+
+    // this.product?.specifications.push(specification);
+    // this.addSpecifications();
   }
 
 }
