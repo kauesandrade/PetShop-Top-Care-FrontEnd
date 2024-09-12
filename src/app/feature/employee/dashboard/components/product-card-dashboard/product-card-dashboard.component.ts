cbac8b6cfa5929b5c0eaf68697f59ecb415c3c2d
Kauê Schaefer de Andrade <128431286+kauesandrade@users.noreply.github.com>
2024-09-12 16:42:54 -0300

    add delete on cardProductCard dashboard

--- src/app/feature/employee/dashboard/components/product-card-dashboard/product-card-dashboard.component.ts
+++ src/app/feature/employee/dashboard/components/product-card-dashboard/product-card-dashboard.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
 import { ProductResponseCard } from 'src/app/shared/interfaces/product/product';
 import { ProductVariant } from 'src/app/shared/interfaces/product/product-variant';
+import { ProductService } from 'src/app/shared/services/product/product.service';
 
 @Component({
   selector: 'app-product-card-dashboard',
@@ -14,13 +15,15 @@ export class ProductCardDashboardComponent implements OnInit {
 
   @Input() product!: ProductResponseCard;
 
-  constructor() { }
+  constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
   }
 
   handleClickTrash(){
-    
+    this.productService.deleteProduct(this.product.code).subscribe(() => {
+      window.location.reload();
+    });
   }
 
 }
