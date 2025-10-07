16e5e9e364b4f21ae911ff425e5f2b3148e36471
Kauê Schaefer de Andrade <128431286+kauesandrade@users.noreply.github.com>
2025-10-06 21:33:11 -0300

    Correção nos imports

--- src/app/shared/components/product-section/product-section.component.ts
+++ src/app/shared/components/product-section/product-section.component.ts
@@ -3,7 +3,7 @@ import { Product } from '../../interfaces/product/product';
 import { ProductVariant } from '../../interfaces/product/product-variant';
 import { FilterService } from '../../services/filter/filter.service';
 import { ProductService } from '../../services/product/product.service';
-import productData from '@assets/JsonFiles/products.json';
+import productData from '@assets/jsonfiles/products.json';
 
 @Component({
   selector: 'app-product-section',
--- src/app/shared/services/petshop/petshop.service.ts
+++ src/app/shared/services/petshop/petshop.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Petshop } from '../../interfaces/petshop/petshop';
-import petshops from '@assets/JsonFiles/petshops.json';
+import petshops from '@assets/jsonfiles/petshops.json';
 
 @Injectable({
   providedIn: 'root',
